5f2b6983b7ddd54d0318ed9d6b9ed4434d0372f9
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-11-27 21:57:56 +0200

    Hero. Part 2

--- components/shared/Button/Button.js
+++ components/shared/Button/Button.js
@@ -1,11 +1,14 @@
 import classNames from 'classnames';
 import AppLink from '../AppLink';
 
-const Button = ({ children, href, theme, ...rest }) => {
-  const classes = classNames('button', { [`button--theme--${theme}`]: theme });
+const Button = ({ children, href, size, theme, ...rest }) => {
+  const classes = classNames('button', { 
+    [`button--theme--${theme}`]: theme,
+    [`button--size--${size}`]: size,
+  });
 
   if (href) {
-    return <AppLink href={href} className={classes}>{children}</AppLink>
+    return <AppLink href={href} className={classes} {...rest}>{children}</AppLink>
   } else {
     return <button className={classes} {...rest}>{children}</button>
   }
--- components/shared/Hero/Hero.js
+++ components/shared/Hero/Hero.js
@@ -1,5 +1,44 @@
+import Button from '../Button';
+
 const Hero = () => (
-  <div className="hero"></div>
+  <div className="hero">
+    <img src='/projects.svg' className="hero__bg-image" alt='projects background' />
+    <div className="hero__container">
+      <div className="hero__info">
+        <p className="hero__title">HELLO!</p>
+        <p className="hero__subtitle">I am Full Stack Javascript Developer.</p>
+        <div>
+          <Button href="/about" size="big">Read more</Button>
+        </div>
+      </div>
+      <div className="hero__cards">
+        <div className="hero__card-wrapper">
+          <div 
+            className="hero__card"
+            style={{ backgroundImage: "url('/proj-1.png')"}}
+          ></div>
+        </div>
+        <div className="hero__card-wrapper">
+          <div 
+            className="hero__card"
+            style={{ backgroundImage: "url('/proj-2.png')"}}
+          ></div>
+        </div>
+        <div className="hero__card-wrapper">
+          <div 
+            className="hero__card"
+            style={{ backgroundImage: "url('/proj-3.png')"}}
+          ></div>
+        </div>
+        <div className="hero__card-wrapper">
+          <div 
+            className="hero__card"
+            style={{ backgroundImage: "url('/proj-4.png')"}}
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Hero;
