f2ad1a8823c175455c2b18652f4ed1dc6ab0214c
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-11-30 15:37:14 +0200

    Change base url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   env: {
-    BASE_URL: isDev ? 'http://localhost:1991/graphql' : 'https://pereverziev.herokuapp.com/graphql'
+    BASE_URL: isDev ? 'http://localhost:1991/graphql' : 'https://pereverziev.com/graphql'
   }
 }
